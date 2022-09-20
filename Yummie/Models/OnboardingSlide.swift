ceccb2d4778cf125114eb511400668ff071037fa
Zehra Karaman <zehrakaramaan@gmail.com>
2022-09-20 23:08:07 +0300

    Add OnboardingSlide model

---
+++ Yummie/Models/OnboardingSlide.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingSlide.swift
+//  Yummie
+//
+//  Created by Zehra on 20.09.2022.
+//
+
+import UIKit
+
+struct OnboardingSlide {
+    let title: String
+    let description: String
+    let image: UIImage
+}
