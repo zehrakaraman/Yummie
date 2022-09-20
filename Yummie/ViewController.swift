99246b753290c84abd7a969d0577f03a12ed647c
Zehra Karaman <zehrakaramaan@gmail.com>
2022-09-20 10:05:20 +0300

    Initial Commit

---
+++ Yummie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yummie
+//
+//  Created by Zehra on 20.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
